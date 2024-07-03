587238ede26ed4e7e63783e8326c3d2004362ea6
shahab <shhm3834@gmail.com>
2024-07-03 14:09:43 +0330

    Add toast for login

--- src/components/SignupLogin.tsx
+++ src/components/SignupLogin.tsx
@@ -2,6 +2,7 @@ import './SignupLogin.css';
 import {Login} from "./Login";
 import {SignUp} from "./SignUp";
 import {useState} from "react";
+import Toast from 'react-bootstrap/Toast'
 
 export const SignupLogin = ({setToken, setTrigger}) => {
     const [name, setName] = useState('');
@@ -9,6 +10,7 @@ export const SignupLogin = ({setToken, setTrigger}) => {
     const [password, setPassword] = useState('');
     const [phone_number, setPhone_number] = useState('');
     const [role, setRole] = useState(false);
+    const [showA, setShowA] = useState(false);
 
     async function signIn(credentials, endpoint) {
         console.log('signing up: ', credentials);
@@ -29,8 +31,12 @@ export const SignupLogin = ({setToken, setTrigger}) => {
             password
         }, 'login');
         console.log('token from backend: ', token);
-        setToken(token);
-        setTrigger(t => !t);
+        if (!token || token.error) {
+            setShowA(s => !s);
+        } else {
+            setToken(token);
+            setTrigger(t => !t);
+        }
     }
 
     const handleSubmitSignup = async (event) => {
@@ -52,28 +58,46 @@ export const SignupLogin = ({setToken, setTrigger}) => {
     const [login, setLogin] = useState(false);
 
     return (
-        <div className="container">
-            <div className="signup-login">
-                {
-                    login ? <Login setUsername={setUsername} setPassword={setPassword} username={username} password={password}/> :
-                        <SignUp password={password} setPassword={setPassword} name={name} setName={setName}
-                                username={username} setUsername={setUsername} phone_number={phone_number}
-                                setPhone_number={setPhone_number} role={role} setRole={setRole}/>
-                }
-                <div>
+        <div>
+            <Toast bg={"danger"} show={showA} onClose={() => {
+                s => !s
+            }}>
+                <Toast.Header>
+                    <img
+                        src="holder.js/20x20?text=%20"
+                        className="rounded me-2"
+                        alt=""
+                    />
+                    <strong className="me-auto">Error</strong>
+                    <small>11 mins ago</small>
+                </Toast.Header>
+                <Toast.Body>username or password is wrong</Toast.Body>
+            </Toast>
+            <div className="container">
+                <div className="signup-login">
                     {
-                        login ? <button className="btn btn btn-success" onClick={handleSubmitLogin}>Login</button> :
-                            <button className="btn btn btn-success" onClick={handleSubmitSignup}>Sign Up</button>
+                        login ? <Login setUsername={setUsername} setPassword={setPassword} username={username}
+                                       password={password}/> :
+                            <SignUp password={password} setPassword={setPassword} name={name} setName={setName}
+                                    username={username} setUsername={setUsername} phone_number={phone_number}
+                                    setPhone_number={setPhone_number} role={role} setRole={setRole}/>
                     }
-                </div>
-                <div>
-                    {
-                        login ? <a className="link" onClick={() => setLogin(false)}>create new account</a> :
-                            <a className="link" onClick={() => setLogin(true)}>already have an account? login</a>
-                    }
-                </div>
-                <div>
+                    <div>
+                        {
+                            login ? <button className="btn btn btn-success" onClick={handleSubmitLogin}>Login</button> :
+                                <button className="btn btn btn-success" onClick={handleSubmitSignup}>Sign Up</button>
+                        }
+                    </div>
+                    <div>
+                        {
+                            login ? <a className="link" onClick={() => setLogin(false)}>create new account</a> :
+                                <a className="link" onClick={() => setLogin(true)}>already have an account? login</a>
+                        }
+                    </div>
+                    <div>
+                    </div>
                 </div>
             </div>
         </div>
-    );};
+    );
+};
